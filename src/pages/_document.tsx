db1928fb12d8b545a50fb58b29d0cf62bb7f2978
Daniel Blazeque <daniel.blazeque@gmail.com>
2023-06-07 22:25:01 -0300

    thumbnail

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <meta property="og:image" content="https://projeto-nutrir.vercel.app/logo-white.png" />
+        <meta name="twitter:image" content="https://projeto-nutrir.vercel.app/logo-white.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
